cebd98a2630168503fba07da24b9b4f3da26cf71
einercupino <einer.cupino@gmail.com>
2023-03-02 17:22:02 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //URI: 'mongodb://127.0.0.1/book_store'
+  URI: "mongodb+srv://einercupino:3eHzZHXjrNWwsXTK@cluster0.kgg0mgj.mongodb.net/?retryWrites=true&w=majority"
+};
---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+//Einer Cupino - 301233614 - COMP229 W2023 Midterms
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the game model
+let book = require('../models/books');
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    books: ''
+   });
+});
+
+module.exports = router;
