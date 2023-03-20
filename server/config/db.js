1471743a3653936e9c54700f7a43da1d83dac12c
KevinD77 <kevindalling@hotmail.com>
2023-03-19 22:16:35 -0400

    Mongo DB updated URI

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,5 @@
 module.exports = {
   //local MongoDB deployment ->
   //URI: 'mongodb://127.0.0.1/comp229test1'
-  URI: "mongodb+srv://einercupino:3eHzZHXjrNWwsXTK@cluster0.kgg0mgj.mongodb.net/?retryWrites=true&w=majority"
+  URI: "mongodb+srv://einercupino:2DLokXhTjGohetuz@cluster0.378e6pu.mongodb.net/?retryWrites=true&w=majority"
 };
